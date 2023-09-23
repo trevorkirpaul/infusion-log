9458ea0e0c607943813ae7ff365a78d30ef328b6
Trevor Kirpaul <trevorkirpaul@gmail.com>
2023-09-23 16:09:27 -0400

    - add authorization to create order endpoint in order to prevent a user from creating an order for another user

--- src/pages/api/orders/create.ts
+++ src/pages/api/orders/create.ts
@@ -13,9 +13,12 @@ export default async function handler(
   if (!session) {
     return res.status(401).send("unauthorized");
   }
+
+  const userIDFromSession: number | null = session?.user?.id || null;
+
   switch (req.method) {
     case "POST":
-      return handlePost(req, res);
+      return handlePost(req, res, userIDFromSession);
     default:
       return res.status(405).json({ message: "Method not allowed" });
   }
@@ -25,9 +28,21 @@ const handleError = (res: NextApiResponse, e: any) => {
   res.status(500).send(e ? e?.details : "error unknown");
 };
 
-const handlePost = async (req: NextApiRequest, res: NextApiResponse) => {
+const handlePost = async (
+  req: NextApiRequest,
+  res: NextApiResponse,
+  userIDFromSession: number | null
+) => {
   try {
     const { body } = req;
+
+    if (userIDFromSession !== Number(body.userID)) {
+      return res
+        .status(400)
+        .send(
+          "Error: You are not authorized to track another user's infusion."
+        );
+    }
 
     if (
       !body.quantity ||
